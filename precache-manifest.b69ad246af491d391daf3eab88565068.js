eb99258653388ee680963ca1ee9641adfe942c06
Aemail <akintomiwa.fisayo@gmail.com>
2019-11-29 21:42:09 +0100

    Updates

---
+++ precache-manifest.b69ad246af491d391daf3eab88565068.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1ab63b92dd91f17f1b607b94b37e784",
+    "url": "/CAPSTONE-frontend/index.html"
+  },
+  {
+    "revision": "c5dee3ab2a00a897bc94",
+    "url": "/CAPSTONE-frontend/static/css/main.d22da8a4.chunk.css"
+  },
+  {
+    "revision": "3ceaaf4d03e130addaac",
+    "url": "/CAPSTONE-frontend/static/js/2.bc201cc8.chunk.js"
+  },
+  {
+    "revision": "c5dee3ab2a00a897bc94",
+    "url": "/CAPSTONE-frontend/static/js/main.05fc055c.chunk.js"
+  },
+  {
+    "revision": "492b5c58151217cc842c",
+    "url": "/CAPSTONE-frontend/static/js/runtime-main.191171a4.js"
+  },
+  {
+    "revision": "9b94499ccea3bd82b24cb210733c4b5e",
+    "url": "/CAPSTONE-frontend/static/media/Pacifico-Regular.9b94499c.ttf"
+  },
+  {
+    "revision": "81cd217e4a8160a930c6d5fb8d1e8e82",
+    "url": "/CAPSTONE-frontend/static/media/SourceSansPro-Light.81cd217e.ttf"
+  },
+  {
+    "revision": "8669b8706bbbdd1482e2fccc4ed96850",
+    "url": "/CAPSTONE-frontend/static/media/SourceSansPro.8669b870.ttf"
+  },
+  {
+    "revision": "9ded577f93b24331f4faadfc5f834ae6",
+    "url": "/CAPSTONE-frontend/static/media/SourceSansProBold.9ded577f.ttf"
+  },
+  {
+    "revision": "5afb4beb84a09836593531e6983cd2e8",
+    "url": "/CAPSTONE-frontend/static/media/coverimage.5afb4beb.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/capstone-frontend/precache-manifest.c787a37ed51938009ad03c9ea01eebd8.js"
+  "/CAPSTONE-frontend/precache-manifest.b69ad246af491d391daf3eab88565068.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/capstone-frontend/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/CAPSTONE-frontend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
